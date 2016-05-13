c8abaf3604fc753008c268b6e1d19d8d0994c9d9
罗旭 <luox199111@gmail.com>
2016-05-13 10:38:15 +0800

    spring hibernate shirt example
    fork from others, and add self-defined authorization filter

---
+++ src/main/java/com/etop/shiro/RolesAnyAuthorizationFilter.java
@@ -0,0 +1,29 @@
+package com.etop.shiro;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+public class RolesAnyAuthorizationFilter extends AuthorizationFilter{
+
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+		Subject subject = getSubject(request, response);
+		String[] rolesArray = (String[]) mappedValue;
+		
+		if(rolesArray == null || rolesArray.length == 0) {
+			return true;
+		}
+		
+		for (int i = 0; i < rolesArray.length; i++) {
+			if(subject.hasRole(rolesArray[i])) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
